b86b660870e67ab1ae25405b0985545cd3b4b1ef
Brandon Morrison <bmorrison@phase2technology.com>
2015-07-16 23:17:51 -0500

    Started work on database scripting

---
+++ server/bootstrap/databaseUpdateManager.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var db = require("./database");
+
+var dbUpdateManager = function() {
+
+};
+
+dbUpdateManager.process = function(args) {
+  console.log("args", args);
+  if (args.length === 0) {
+    console.log(this.instructions());
+  } else if (args[0] == "update") {
+    this.update();
+  }
+};
+
+dbUpdateManager.instructions = function() {
+  return "HI";
+};
+
+dbUpdateManager.update = function() {
+  db.one("SELECT EXISTS(SELECT * FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'var');")
+    .then(function(data) {
+      if (data.exists === true) {
+        console.log("It works");
+      }
+      process.exit(0);
+    });
+  //db.query("CREATE TABLE var (name  varchar(128), val jsonb)")
+  //  .then(function(data) {
+  //    console.log(data);
+  //    return;
+  //  });
+};
+
+module.exports = dbUpdateManager;
